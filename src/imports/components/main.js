6faec4c0ea2ad2595c17a26604b75be49678b35c
theory friction practice <jackson.brebnor@gmail.com>
2016-11-19 22:01:50 -0500

    redux dependencies

--- src/imports/components/main.js
+++ src/imports/components/main.js
@@ -1,10 +1,10 @@
-import React, {Component, PropTypes} from 'react';
+import React, {Component, PropTypes,} from 'react';
 
 class Main extends Component {
     render() {
         return (
             <div id='main-view' className='container'>
-              <h1>I AM THE MAIN Chnaged Component</h1>
+              <h1>I AM THE MAIN Component</h1>
                 <hr/> {this.props.children}
             </div>
         );
