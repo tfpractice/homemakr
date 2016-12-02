9b2a7bf298d89c56ab9df7cdd7bdc582a4e1bff3
theory friction practice <jackson.brebnor@gmail.com>
2016-12-02 12:43:00 -0500

    prerefactor

--- src/imports/tasks/components/list.js
+++ src/imports/tasks/components/list.js
@@ -9,6 +9,28 @@ import { Tabs, Tab, } from 'material-ui/Tabs';
 import { reset, } from 'redux-form';
 import TaskForm, { EditForm, } from './form';
 
+import { Card, CardActions, CardHeader, CardText, } from 'material-ui/Card';
+
+const CardExampleExpandable = ({ task, editable, actions, }) => (
+  <Card>
+    <CardHeader
+      title={task.text}
+      subtitle={task.dateAdded}
+      actAsExpander
+      showExpandableButton
+    />
+    {editable ? <CardText expandable>
+      <EditForm
+        key={task.id}
+        form={`edit_form${task.id}`}
+        task={task}
+        initialValues={task}
+        onSubmit={actions.editTask(task)}
+        onSubmitSuccess={resetForm(`edit_form${task.id}`)}
+      /></CardText> : ''}
+  </Card>
+);
+
 const resetForm = name => (action, dispatch) => dispatch(reset(name));
 
 { /* <EditForm
@@ -25,13 +47,13 @@ const mapStateToProps = ({ auth: { user, }, }, { task, }) =>
 
 const TaskC = ({ actions, task, user, editable, }) => {
   console.log('task editable', editable);
-
+  return CardExampleExpandable({ task, editable, actions, });
   return (
     <li >
       <div className="collapsible-header">
         <p> task: {task.text} </p>
         {task.author.username ? <p> author: {task.author.username} </p> : null}
-        
+
       </div>
       <div className="collapsible-body">
         {!editable ? '' :
@@ -51,11 +73,11 @@ const TaskC = ({ actions, task, user, editable, }) => {
 const Task = connect(mapStateToProps)(TaskC);
 
 const TaskList = ({ actions, tasks, }) => (
-  <ul className="collapsible" data-collapsible="accordion">
+  <List>
     {tasks.map((task, index) =>
       <Task key={index} task={task} actions={actions} />
     )}
-  </ul>
+  </List>
     );
 
 export default TaskList;
