936182b11fdb4cf086ab5eb9378a43742e1480e4
theory friction practice <jackson.brebnor@gmail.com>
2016-12-02 18:42:39 -0500

    tasks/actions exports only async

--- src/imports/modules/tasks/actions.js
+++ src/imports/modules/tasks/actions.js
@@ -16,18 +16,28 @@ export const setTaskFilter = filter =>
 
 const update = newTasks => tasks => newTasks;
 const insert = task => tasks => tasks.concat(task);
+const remove = ({ id, }) => tasks => tasks.filter(t => t.id !== id);
 const edit = task => tasks =>
   tasks.map(t => t.id === task.id ? { ...t, ...task, } : t);
 
-const remove = ({ id, }) => tasks => tasks.filter(t => t.id !== id);
+const insertTask = task =>
+({ type: INSERT_TASK, curry: insert(task), });
 
-export const taskRequestSucess = () =>
+const updateTasks = tasks =>
+ ({ type: UPDATE_TASKS, curry: update(tasks), });
+
+const updateTask = task =>
+ ({ type: EDIT_TASK, curry: edit(task), });
+
+const taskRequestSucess = () =>
   ({ type: TASK_REQUEST_SUCCESS, curry: success, });
 
-export const taskRequestFailure = err =>
+const taskRequestFailure = err =>
   ({ type: TASK_REQUEST_FAILURE, curry: failure, });
 
-export const updateTasks = tasks => ({ type: UPDATE_TASKS, curry: update(tasks), });
+const removeTask = ({ id, }) =>
+ ({ type:  DELETE_TASK, curry: remove({ id, }), });
+
 export const getTasks = () => (dispatch) => {
   dispatch({ type: TASK_REQUEST_PENDING, curry: pending, });
   return axios.get(`${API_URL}/tasks`)
@@ -36,8 +46,6 @@ export const getTasks = () => (dispatch) => {
     .catch(taskRequestFailure);
 };
 
-export const insertTask = task => ({ type: INSERT_TASK, curry: insert(task), });
-
 export const createTask = taskProps => (dispatch) => {
   console.log('========tasksprops======', taskProps);
   return axios.post(`${API_URL}/tasks`, taskProps)
@@ -45,17 +53,10 @@ export const createTask = taskProps => (dispatch) => {
     .catch(err => console.error('there was an error in creation', err));
 };
 
-export const updateTask = task => ({ type: EDIT_TASK, curry: edit(task), });
-
 export const editTask = ({ id, }) => dispatch => taskProps =>
  axios.patch(`${API_URL}/tasks/${id}`, taskProps)
    .then(({ data: { task, }, }) => dispatch(updateTask(task)))
    .catch(err => console.error('there was an error in update', err));
-
-export const removeTask = ({ id, }) => ({
-  type:  DELETE_TASK,
-  curry: remove({ id, }),
-});
 
 export const deleteTask = ({ id, }) => dispatch =>
   axios.delete(`${API_URL}/tasks/${id}`)
