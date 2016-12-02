12287fae11abcebafe80a668f59dcd286e822f7c
theory friction practice <jackson.brebnor@gmail.com>
2016-12-02 13:49:40 -0500

    deletes imports/tasks

--- src/imports/root_reducer.js
+++ src/imports/root_reducer.js
@@ -1,8 +1,6 @@
 import { reducer as form, } from 'redux-form';
+import { combineReducers, applyMiddleware, } from 'redux';
 import { reducer as tasks, tasksReducer, } from './modules/tasks';
 import { reducer as auth, } from './modules/auth';
-import { combineReducers, applyMiddleware, } from 'redux';
 
-const rootReducer = combineReducers({ form, tasks, tasksReducer, auth, });
-
-export default rootReducer;
+export default combineReducers({ form, tasks, tasksReducer, auth, });
--- src/imports/tasks/index.js
+++
@@ -1,6 +0,0 @@
-import * as actions from './actions';
-import TasksView from './components';
-import reducer, { tasksReducer, } from './reducer';
-console.log(__filename, '\n ========TASK DEPEND=====\n', module.parent);
-
-export { actions, reducer, tasksReducer, TasksView, };
