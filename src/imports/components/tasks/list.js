ad36342dcc49bf7e562bbcbd3349825b1a36b2c3
theory friction practice <jackson.brebnor@gmail.com>
2016-12-04 17:37:57 -0500

    fixes expanding edit

--- src/imports/components/tasks/list.js
+++ src/imports/components/tasks/list.js
@@ -14,32 +14,35 @@ import TaskForm, { EditForm, } from './form';
 import Accordion from 'grommet/components/Accordion';
 import AccordionPanel from 'grommet/components/AccordionPanel';
 
+const CardExampleExpandable = ({ task, editable, actions, }) => {
+
+};
+
 const resetForm = name => (action, dispatch) => dispatch(reset(name));
 
 const mapStateToProps = ({ auth: { user, }, }, { task, }) =>
     ({ editable: user && task.author === user.id, });
 
-const TaskC = ({ actions, task, user, editable, }) => (
-
-  <EditForm
-    key={task.id}
-    form={`edit_form${task.id}`}
-    task={task}
-    initialValues={task}
-    onSubmit={actions.editTask(task)}
-    onSubmitSuccess={resetForm(`edit_form${task.id}`)}
-  />
-  );
+const TaskC = ({ actions, task, user, editable, }) => (editable ? <EditForm
+  key={task.id}
+  form={`edit_form${task.id}`}
+  task={task}
+  initialValues={task}
+  onSubmit={actions.editTask(task)}
+  onSubmitSuccess={resetForm(`edit_form${task.id}`)}
+/> : '');
 
 const Task = connect(mapStateToProps)(TaskC);
 
 const TaskList = ({ actions, tasks, }) => (
   <Accordion openMulti>
     {tasks.map((task, index) =>
-      <AccordionPanel key={task.id} heading={`title: ${task.text}`}>
+      <AccordionPanel key={task.id} heading={`title: ${task.text} `}>
         <Task key={task.id} task={task} actions={actions} />
-      </AccordionPanel>
-    )}
-  </Accordion>);
+      </AccordionPanel>)}
+
+  </Accordion>
+
+    );
 
 export default TaskList;
